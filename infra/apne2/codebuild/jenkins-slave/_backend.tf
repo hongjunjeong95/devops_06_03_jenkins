287dcff8708a46ccfc462c5bf78f250770200f78
hongjunjeong95 <hongjunjeong95@gmail.com>
2021-12-08 10:52:52 +0900

    Amend s3 bucket path

--- infra/apne2/codebuild/jenkins-slave/_backend.tf
+++ infra/apne2/codebuild/jenkins-slave/_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket      = "devops-dev-tfbackend-s3"
+    bucket      = "devops-dev-tfbackend-s3-lewisjeong"
     key         = "dev/apne2/codebuild/jenkins-slave/terraform.tfstate"
     region      = "ap-northeast-2"
 #    role_arn    = "{ASSUMED_ROLE}"
